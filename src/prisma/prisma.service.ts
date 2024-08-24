a6bc032eec08b97a6d0985ce2ed6b20df84ccdc7
alimarief <alimarief97@gmail.com>
2024-08-24 12:25:36 +0700

    fix: prisma add binaryTarget

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,11 +6,19 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'], // Enable detailed logging
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+    console.log('Prisma service initialized and connected.');
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+    console.log('Prisma service disconnected.');
   }
 }

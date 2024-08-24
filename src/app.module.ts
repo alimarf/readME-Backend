3bb76506f200ce4d7301e2e0304d57438aec8a4d
alimarief <alimarief97@gmail.com>
2024-08-24 12:15:01 +0700

    feat: add prismamodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     SupabaseModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],

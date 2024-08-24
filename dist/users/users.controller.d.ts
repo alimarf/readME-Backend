46dfc9742d026745466971a5a33190055becae26
alimarief <alimarief97@gmail.com>
2024-08-24 13:07:55 +0700

    fix: command build & vercel.json

--- dist/users/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserEntity } from './entities/user.entity';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    create(createUserDto: CreateUserDto): Promise<UserEntity>;
-    findAll(): Promise<UserEntity[]>;
-}
